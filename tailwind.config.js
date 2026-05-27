7113b48e7bc1027dee8db42877a42f9f3e39f413
Daniel Ruiz Vargas <199542025+danielruiz1208@users.noreply.github.com>
2026-05-27 19:57:39 -0400

    Update Tailwind CSS configuration for content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.js',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './app/**/*.php',
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Outfit', ...defaultTheme.fontFamily.sans],
             },
         },
     },
