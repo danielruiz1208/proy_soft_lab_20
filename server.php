3656727e1f6f7d0f038d935a5560fec6d4c46c5c
Daniel Ruiz Vargas <199542025+danielruiz1208@users.noreply.github.com>
2026-05-27 20:31:37 -0400

    Check if file exists using is_file function

--- server.php
+++ server.php
@@ -1,8 +1,9 @@
 <?php
 
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$file = __DIR__ . '/public' . $uri;
 
-if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+if ($uri !== '/' && is_file($file)) {
     return false;
 }
 
