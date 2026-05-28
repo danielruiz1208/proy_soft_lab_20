b57333830ec1bfe3b1700ec318092c2b924047ea
Daniel Ruiz Vargas <199542025+danielruiz1208@users.noreply.github.com>
2026-05-27 22:39:20 -0400

    Refactor User model attributes and properties

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,25 +2,30 @@
 
 namespace App\Models;
 
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'user', 'email', 'password', 'rol'])]
-#[Hidden(['password'])]
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
     public $timestamps = false;
 
-    protected function casts(): array
-    {
-        return [
-            'password' => 'hashed',
-        ];
-    }
+    protected $fillable = [
+        'name',
+        'user',
+        'email',
+        'password',
+        'rol',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
 }
