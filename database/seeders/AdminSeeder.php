0cad6cdb884e8217fd9c28532bfb8b09cd109cdb
Daniel Ruiz Vargas <199542025+danielruiz1208@users.noreply.github.com>
2026-05-27 22:51:06 -0400

    Create AdminSeeder to seed admin user

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['user' => 'admin'],
+            [
+                'name' => 'Administrador',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('12345678'),
+                'rol' => 'admin',
+            ]
+        );
+    }
+}
